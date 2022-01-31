3b53e2b5dbe0d34203adbf3ec7af0c38c1d737df
Your Name <you@example.com>
2022-02-01 00:37:06 +0530

    updated conversation

--- Models/Conversation.js
+++ Models/Conversation.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose') ;
 const ConversationSchema = new mongoose.Schema({
-   members:[String]
-},{
+   members:[String] ,
+   signal:[{
+      type:Number,
+      default:0
+   }]
+},
+{
     writeConcern: {
        w: 'majority',
        j: true,
