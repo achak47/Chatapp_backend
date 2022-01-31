b4df363f1d897afacf9012d96874f93cefc86b3a
Your Name <you@example.com>
2022-02-01 01:18:37 +0530

    qwerty3

--- routes/conversations.js
+++ routes/conversations.js
@@ -3,9 +3,8 @@ const Conversation = require("../Models/Conversation");
 
 //new conversations
 router.post("/",(req,res)=>{
-    var arr = [req.body.receiverId , req.body.senderId] ;
-    Conversation.find({members:{ $all: [req.body.receiverId , req.body.senderId] }},(err,result)=>{
-        if(result.length>0) res.status(200).json("Added Conversation") ;
+   const result = await Conversation.findOne({members:{ $all: [req.body.receiverId , req.body.senderId] }})
+        if(result) res.status(200).json("Added Conversation") ;
         else{
             new Conversation({
                 members:[req.body.senderId, req.body.receiverId]
@@ -14,7 +13,6 @@ router.post("/",(req,res)=>{
                 else res.status(200).json(result) ;
             }) ;
         }
-    })
 
 })
 //get old conversations of an user
